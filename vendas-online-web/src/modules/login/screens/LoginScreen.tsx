bcee14ea43b419eb3623efcda9a70f4a0bcac023
Marcio Carolino <marciocarolino@gmail.com>
2023-06-15 10:46:13 -0300

    feat: create login component

--- vendas-online-web/src/modules/login/screens/LoginScreen.tsx
+++ vendas-online-web/src/modules/login/screens/LoginScreen.tsx
@@ -1,9 +1,22 @@
-import { BackgroundImage } from "../styles/loginScreen.styles";
+import {
+  BackgroundImage,
+  ConatinerLogin,
+  ConatinerLoginScreen,
+  LimitedContainer,
+  LogoImage,
+} from "../styles/loginScreen.styles";
 
 const LoginScreen = () => {
   return (
     <>
-      <BackgroundImage src="./background.png" />
+      <ConatinerLoginScreen>
+        <ConatinerLogin>
+          <LimitedContainer>
+            <LogoImage src="" />
+          </LimitedContainer>
+        </ConatinerLogin>
+        <BackgroundImage src="./background.png" />
+      </ConatinerLoginScreen>
     </>
   );
 };
